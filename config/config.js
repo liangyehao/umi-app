67f9340bcd55f1aa1316d5e183407b20650a2e87
liangyehao@huawei <1094311509@qq.com>
2020-04-20 09:14:54 +0800

    主干去掉config多余注释

--- config/config.js
+++ config/config.js
@@ -1,18 +1,6 @@
 /**
  * 代理配置
  */
-// const proxy = {
-//     '/api': {
-//         target: 'http://pv.sohu.com', //  需修改为真实后端服务地址
-//         changeOrigin: true,
-//         pathRewrite: { "^/api" : "" }
-//     },
-//     '/github': {
-//         target: 'https://api.github.com', //  需修改为真实后端服务地址
-//         changeOrigin: true,
-//         pathRewrite: { "^/github" : "" }
-//     },
-// };
 
 import {proxy} from './custom.config.js'
 
